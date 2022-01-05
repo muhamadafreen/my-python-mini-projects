ccf8d69789fe913d1482d5e9fa77921e69e4288c
Muhamad Afreen <muhamadafreen111@gmail.com>
2022-01-05 12:26:48 +0530

    This my second commit,coded upto generating a random number using python function rand

--- mini_project 1 number guessing.py
+++ mini_project 1 number guessing.py
@@ -12,3 +12,28 @@ else:
     print("Enter a number next time ")
     quit()
     
+
+random_number = random.randint(0,top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Enter a guess: ")
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else:
+        print("Enter a number") 
+        continue
+
+    # if user_guess == random_number:
+    #     print("WoW,you got it right")   
+
+    #     break
+
+    # elif user_guess > random_number:
+    #     print("You were above the number")
+
+    # else:
+    #     print("you were below the number")    
